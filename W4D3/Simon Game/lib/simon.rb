7ab6a8a93781004548035d86d70be62f3fc15499
Sebastian Sanchez <sebastian@artxcode.io>
2021-03-16 19:47:40 -0400

    Added user play to the game

--- W4D3/Simon Game/lib/simon.rb
+++ W4D3/Simon Game/lib/simon.rb
@@ -11,28 +11,46 @@ class Simon
   end
 
   def play
-    self.take_turn until @game_over
-    if @game_over 
-      self.game_over_message
-      self.reset_game
+    until @game_over
+      take_turn 
+      system "clear"
     end 
+
+      game_over_message
+      reset_game
+
   end
 
   def take_turn
-    self.show_sequence
-    self.require_sequence
+    show_sequence
+    require_sequence
 
     if !@game_over
-      self.round_success_message 
+      round_success_message 
       @sequence_length += 1
     end 
   end
 
   def show_sequence
-    self.add_random_color
+    add_random_color
+    seq.each do |color|
+      puts color
+      sleep 0.75
+      system ("clear")
+      sleep 0.25
+    end 
   end
 
   def require_sequence
+    "Repeat the sequence by entering the first letter of each color on a new line"
+     @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
 
   end
 
@@ -44,11 +62,12 @@ class Simon
   end
 
   def round_success_message
+    "Round successful. Here's the next sequence:"
 
   end
 
   def game_over_message
-
+    puts "Game over! You made it #{@sequence_length - 1} rounds."
   end
 
   def reset_game
